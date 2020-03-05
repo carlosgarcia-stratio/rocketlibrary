c26dafa38e66cbd978cccc1219d06fcf27a96b26
Carlos Garcia <carlosgarcia@stratio.com>
2020-03-05 20:02:09 +0100

    First commit

--- vars/releaseRocketWorkflow.groovy
+++ vars/releaseRocketWorkflow.groovy
@@ -1,6 +1,10 @@
 #!groovy
+import com.stratio.rocket.rocketUtils.RocketConstants
 
 def call(Map props = [:]) {
+
+    env[RocketConstants.ROCKET_API_URL_DEV] = "localhost:8080"
+    env[RocketConstants.ROCKET_API_URL_PRO] = "localhost:8081"
 
     println(props)
     service.initRocketInstances()

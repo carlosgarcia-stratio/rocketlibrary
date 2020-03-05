585d7808f2df55e53ad0da59b49d5304053c45bd
Carlos Garcia <carlosgarcia@stratio.com>
2020-03-05 19:31:59 +0100

    First commit

--- vars/releaseRocketWorkflow.groovy
+++ vars/releaseRocketWorkflow.groovy
@@ -1,5 +1,5 @@
 
 def call(Map props = [:]) {
 
-    (props)
+    println(props)
 }
--- vars/service.groovy
+++ vars/service.groovy
@@ -4,6 +4,6 @@ import groovy.transform.Field
 import com.stratio.rocket.rocketUtils.RocketApi
 import com.stratio.rocket.rocketUtils.RocketConstants
 
-@Field RocketApi rocketDev = new RocketApi(RocketConstants.ROCKET_API_URL_DEV)
-@Field RocketApi rocketPre = new RocketApi(RocketConstants.ROCKET_API_URL_PRE)
-@Field RocketApi rocketPro = new RocketApi(RocketConstants.ROCKET_API_URL_PRO)
+@Field static RocketApi rocketDev = new RocketApi(RocketConstants.ROCKET_API_URL_DEV)
+@Field static RocketApi rocketPre = new RocketApi(RocketConstants.ROCKET_API_URL_PRE)
+@Field static RocketApi rocketPro = new RocketApi(RocketConstants.ROCKET_API_URL_PRO)

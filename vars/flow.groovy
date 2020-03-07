05b63013f5cf54c1b838293d2916111918403eef
Carlos Garcia <carlosgarcia@stratio.com>
2020-03-07 22:19:09 +0100

    First commit

--- vars/flow.groovy
+++ vars/flow.groovy
@@ -2,6 +2,7 @@
 
 import groovy.transform.Field
 import com.stratio.rocket.flow.FlowBuilder
+import com.stratio.rocket.flow.FlowExecutor
 
 @Field static def builder = new FlowBuilder()
 @Field static def executor = new FlowExecutor()

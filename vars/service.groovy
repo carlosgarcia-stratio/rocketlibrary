65c6a7c3597b6b07f2d9ea952d7f374576b04124
Carlos Garcia <carlosgarcia@stratio.com>
2020-03-07 11:01:44 +0100

    First commit

--- vars/service.groovy
+++ vars/service.groovy
@@ -1,7 +1,6 @@
 #!groovy
 
 import groovy.transform.Field
-import com.stratio.rocket.rocketUtils.RocketApi
 import com.stratio.rocket.rocketUtils.RocketConstants
 
 @Field static def instances = [:]
@@ -9,14 +8,14 @@ import com.stratio.rocket.rocketUtils.RocketConstants
 def initRocketInstances() {
 
     if (env[RocketConstants.ROCKET_API_URL_DEV]) {
-      instances[RocketConstants.DEV] = new RocketApi(env[RocketConstants.ROCKET_API_URL_DEV])
+      instances[RocketConstants.DEV] = env[RocketConstants.ROCKET_API_URL_DEV]
     }
 
     if (env[RocketConstants.ROCKET_API_URL_DEV]) {
-        instances[RocketConstants.PRE] = new RocketApi(env[RocketConstants.ROCKET_API_URL_PRE])
+        instances[RocketConstants.PRE] = env[RocketConstants.ROCKET_API_URL_DEV]
     }
 
     if (env[RocketConstants.ROCKET_API_URL_DEV]) {
-        instances[RocketConstants.PRO] = new RocketApi(env[RocketConstants.ROCKET_API_URL_PRO])
+        instances[RocketConstants.PRO] = env[RocketConstants.ROCKET_API_URL_DEV]
     }
 }

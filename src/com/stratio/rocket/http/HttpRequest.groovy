1c8cf7d5396d6b4b5e13bc18d5b6799c5fffa2e0
Carlos Garcia <carlosgarcia@stratio.com>
2020-03-07 11:19:10 +0100

    First commit

--- src/com/stratio/rocket/http/HttpRequest.groovy
+++ src/com/stratio/rocket/http/HttpRequest.groovy
@@ -6,30 +6,36 @@ class HttpRequest implements Serializable {
     
     HttpRequest() {
         this.command = "curl"
+        println(this.command)
     }
 
     HttpRequest withUrl(String url) {
         this.command += " " + url
+        println(this.command)
         return this
     }
 
     HttpRequest get() {
         this.command += " -XGET"
+        println(this.command)
         return this
     }
 
     HttpRequest post() {
         this.command += " -XPOST"
+        println(this.command)
         return this
     }
 
     HttpRequest put() {
         this.command += " -XPUT"
+        println(this.command)
         return this
     }
 
     HttpRequest delete() {
         this.command += " -XDELETE"
+        println(this.command)
         return this
     }
     
@@ -37,27 +43,32 @@ class HttpRequest implements Serializable {
         if(flag) {
             this.command += " -vvv "
         }
+        println(this.command)
         return this
     }
 
     HttpRequest withHeader(String header) {
         this.command += " -H '" + aHeader + "'"
+        println(this.command)
         return this
     }
 
     HttpRequest insecure() {
         this.command += " -k"
+        println(this.command)
         return this
     }
 
     HttpRequest silent() {
         this.command += " -s"
+        println(this.command)
         return this
     }
 
 
     HttpRequest withCredentials(String aCredentials) {
         this.command += " -u '" + aCredentials + "'"
+        println(this.command)
         return this
     }
 
@@ -66,11 +77,13 @@ class HttpRequest implements Serializable {
             return this
         }
         this.command += " -d '" + body + "'"
+        println(this.command)
         return this
     }
 
     HttpRequest upload(String file) {
         this.command += " -T " + file
+        println(this.command)
         return this
     }
 

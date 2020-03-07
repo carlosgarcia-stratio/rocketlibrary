bd0d2bec57d9960fbd89a39e4f55425831367444
Carlos Garcia <carlosgarcia@stratio.com>
2020-03-07 22:22:13 +0100

    First commit

--- src/com/stratio/rocket/flow/FlowExecutor.groovy
+++ src/com/stratio/rocket/flow/FlowExecutor.groovy
@@ -1,12 +1,14 @@
 package com.stratio.rocket.flow
 
 def execute(flow) {
+    println(flow)
     flow.each { stage ->
         executeStage(stage)
     }
 }
 
 def executeStage(stage){
+    println(stage)
     stage.executeStage()
 }
 

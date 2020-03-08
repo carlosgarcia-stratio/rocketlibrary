2e6ca5bcea639ac80607359c591dd681c1e1df1c
Carlos Garcia <carlosgarcia@stratio.com>
2020-03-08 09:15:01 +0100

    First commit

--- vars/context.groovy
+++ vars/context.groovy
@@ -5,7 +5,11 @@ import groovy.transform.Field
 @Field def props = [:]
 
 def init(Map p) {
+    println(p)
     p.each { key ->
+        println(key)
+        println(p[key])
         props[key] = p[key]
     }
+    println(props)
 }
--- vars/start.groovy
+++ vars/start.groovy
@@ -6,7 +6,9 @@ def call(Map props = [:]) {
     env[RocketConstants.ROCKET_API_URL_DEV] = "172.17.0.5:9090"
     env[RocketConstants.ROCKET_API_URL_PRO] = "localhost:9091"
 
+    println(props)
     context.init(props)
+    println(context.props)
     rocket.initInstances()
     flow.executor.execute(flow.builder.create())
 }

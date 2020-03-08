102724b549263673ced1e9dca6fe69b1c19b7640
Carlos Garcia <carlosgarcia@stratio.com>
2020-03-08 09:17:52 +0100

    First commit

--- vars/context.groovy
+++ vars/context.groovy
@@ -5,11 +5,5 @@ import groovy.transform.Field
 @Field def props = [:]
 
 def init(Map p) {
-    println(p)
-    p.each { key ->
-        println(key)
-        println(p[key])
-        props[key] = p[key]
-    }
-    println(props)
+    props = p.clone()
 }

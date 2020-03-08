b421d016160cc0c3378f37ebd68643c7eef0fdc4
Carlos Garcia <carlosgarcia@stratio.com>
2020-03-08 09:08:38 +0100

    First commit

--- vars/start.groovy
+++ vars/start.groovy
@@ -3,8 +3,8 @@ import com.stratio.rocket.rocketUtils.RocketConstants
 
 def call(Map props = [:]) {
 
-    env[RocketConstants.ROCKET_API_URL_DEV] = "172.17.0.5:8080"
-    env[RocketConstants.ROCKET_API_URL_PRO] = "localhost:8081"
+    env[RocketConstants.ROCKET_API_URL_DEV] = "172.17.0.5:9090"
+    env[RocketConstants.ROCKET_API_URL_PRO] = "localhost:9091"
 
     context.props = props
     rocket.initInstances()

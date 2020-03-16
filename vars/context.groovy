f997236737ab351b8a6620731da6ae9cbafb374b
Carlos Garcia <carlosgarcia@stratio.com>
2020-03-16 13:37:08 +0100

    First commit

--- vars/context.groovy
+++ vars/context.groovy
@@ -1,12 +1,8 @@
 #!groovy
 
 import groovy.transform.Field
-import com.stratio.rocket.rocketUtils.Workflow
-import com.stratio.rocket.rocketUtils.Project
 
 @Field def static props = [:]
-@Field def static workflowDev = new Workflow()
-@Field def static project = new Project()
 
 def init(Map p) {
     props << p

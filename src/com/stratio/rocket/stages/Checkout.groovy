5b0a5e0276c5b8e368e70c6d18f29df8750a6af7
Carlos Garcia <carlosgarcia@stratio.com>
2020-03-16 13:35:09 +0100

    First commit

--- src/com/stratio/rocket/stages/Checkout.groovy
+++ src/com/stratio/rocket/stages/Checkout.groovy
@@ -14,9 +14,9 @@ def executeStage() {
 def execute() {
     log.info("Checkout Stages execute")
     def workflow = rocket.dev.api.getWorkflow(context.props["workflowId"])
-    context.dev.workflow.init(workflow, readJSON(text: workflow), context.props["releaseId"])
+    rocket.dev.workflow.init(workflow, readJSON(text: workflow), context.props["releaseId"])
     def project = rocket.dev.api.getProject(context.workflow.getProjectId())
-    context.dev.project.init(project, readJSON(text: project))
+    rocket.dev.project.init(project, readJSON(text: project))
     //def folders = jsonWorkflow["group"]["name"].split("/").findAll{ !(it == '' || it == 'home' || it == jsonProject["name"]) }
     sleep 1
 }

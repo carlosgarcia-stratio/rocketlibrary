2e7656bfa5863d533294714b7d4998963efcdc0d
Carlos Garcia <carlosgarcia@stratio.com>
2020-03-25 11:46:55 +0100

    First commit

--- src/com/stratio/rocket/stages/PostStage.groovy
+++ src/com/stratio/rocket/stages/PostStage.groovy
@@ -7,7 +7,8 @@ def executeStage(s) {
     if(s.status == "FAILED"){
         rocket.dev.addReleaseStageState(s.name, RocketConstants.STAGE_FAILED, "Stage ${s.name} failed: ${s.message}.")
     } else {
-        rocket.dev.addReleaseStageState(s.name, RocketConstants.STAGE_FINISHED, "Stage ${s.name} finished.")
+        def message = s.message == "" ? "Stage ${s.name} finished successfully" : s.message
+        rocket.dev.addReleaseStageState(s.name, RocketConstants.STAGE_FINISHED, message)
     }
 
 }

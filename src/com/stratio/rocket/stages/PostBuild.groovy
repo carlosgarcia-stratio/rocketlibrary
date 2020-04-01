13fd3e1d5818762745ca6e1ea7a14c20e6075d69
Carlos Garcia <carlosgarcia@stratio.com>
2020-04-01 20:43:57 +0200

    First Commit

--- src/com/stratio/rocket/stages/PostBuild.groovy
+++ src/com/stratio/rocket/stages/PostBuild.groovy
@@ -7,7 +7,7 @@ def executeStage() {
     println(context.ctx.buildStatus)
     if(context.ctx.buildStatus == FlowConstants.FAILURE) {
         rocket.dev.addReleaseInfo("Duration", "${currentBuild.duration/1000} seconds")
-        rocket.dev.addReleaseInfo("Error", context.error)
+        rocket.dev.addReleaseInfo("Error", context.ctx.error)
         rocket.dev.updateReleaseExecutionState(RocketConstants.RELEASE_FAILED)
     } else {
         rocket.dev.addReleaseInfo("Duration", "${currentBuild.duration/1000} seconds")

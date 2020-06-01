aa9cfd995e25bc73d6f4703178523423049ab758
Carlos Garcia <carlosgarcia@stratio.com>
2020-06-01 19:31:09 +0200

    test

--- vars/doWorkflow2envs.groovy
+++ vars/doWorkflow2envs.groovy
@@ -2,9 +2,8 @@
 
 def call(Map props = [:]) {
 
-    node("maven") {
+    node() {
         println("Rocket flow for workflow 2 envs")
-
 
         stage('Checkout Workflow') {
             sh 'mvn --version'

5e9f3a268945e44811a449e02bb41896c201611e
Carlos Garcia <carlosgarcia@stratio.com>
2020-06-01 23:27:56 +0200

    test

--- vars/doWorkflow2envs.groovy
+++ vars/doWorkflow2envs.groovy
@@ -3,10 +3,11 @@
 def call(Map props = [:]) {
 
     node("rocket") {
+
         println("Rocket flow for workflow 2 envs")
 
         stage('Checkout Workflow') {
-            sh 'mvn --version'
+            sh 'mvn com.stratio.rocket:rocket-maven-plugin:0.1.0-SNAPSHOT:checkout -DrocketBaseUrl=http://localhost:9090 -Dcookie="" -DassetId="a6e5049e-87b2-4cad-ae09-0150f2f56a95"'
         }
 
         stage('QA') {

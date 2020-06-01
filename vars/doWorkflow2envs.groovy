3d60d72fa263e5a57efa8aada15ae3a2564f6409
Carlos Garcia <carlosgarcia@stratio.com>
2020-06-01 18:18:25 +0200

    Clean flow code

--- vars/doWorkflow2envs.groovy
+++ vars/doWorkflow2envs.groovy
@@ -5,22 +5,18 @@ def call(Map props = [:]) {
     node {
         println("Rocket flow for workflow 2 envs")
 
-        stages {
 
-            stage('Checkout Workflow') {
-                sh 'Checkwork workflow'
-            }
-
-            stage('QA') {
-                sh 'QA'
-            }
-
-            stage('Deploy') {
-                sh 'Deploy'
-            }
-
+        stage('Checkout Workflow') {
+            sh 'Checkwork workflow'
         }
 
+        stage('QA') {
+            sh 'QA'
+        }
+
+        stage('Deploy') {
+            sh 'Deploy'
+        }
     }
 
 

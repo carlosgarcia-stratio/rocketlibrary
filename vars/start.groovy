9df91fb4de39072a4987cfd7bd643a3f3c39e802
Carlos Garcia <carlosgarcia@stratio.com>
2020-06-10 20:24:42 +0200

    Add basic selector

--- vars/start.groovy
+++ vars/start.groovy
@@ -5,13 +5,15 @@ def call(Map props = [:]) {
     if (props["assetType"] == "Workflow") {
         doWorkflow2envs(props)
     } else if (props["assetType"] == "MLModel") {
-
+        doMLModel2envs(props)
     } else if (props["assetType"] == "MLProject") {
-
+        doMLProject2envs(props)
     } else if (props["assetType"] == "AutoML") {
-
+        doAutoML2envs(props)
     } else if (props["assetType"] == "MLTrainer") {
-
+        doMLTrainer2envs(props)
+    } else {
+        error "Invalid asset type ${props['assetType']}"
     }
 
 

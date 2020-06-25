5f2c8e0a8a8df7ff041b8a401337d29b2d9ee45f
Carlos Garcia <carlosgarcia@stratio.com>
2020-06-25 22:53:52 +0200

    Add console color

--- vars/start.groovy
+++ vars/start.groovy
@@ -2,17 +2,20 @@
 
 def call(Map props = [:]) {
 
-    if (props["assetType"] == "Workflow") {
-        doWorkflow2envs(props)
-    } else if (props["assetType"] == "MlModel") {
-        doMLModel2envs(props)
-    } else if (props["assetType"] == "MlFlowProject") {
-        doMLProject2envs(props)
-    } else if (props["assetType"] == "AutoMl") {
-        doAutoML2envs(props)
-    } else if (props["assetType"] == "MlTrainer") {
-        doMLTrainer2envs(props)
-    } else {
-        error "Invalid asset type ${props['assetType']}"
+    ansicolor('xterm') {
+
+        if (props["assetType"] == "Workflow") {
+            doWorkflow2envs(props)
+        } else if (props["assetType"] == "MlModel") {
+            doMLModel2envs(props)
+        } else if (props["assetType"] == "MlFlowProject") {
+            doMLProject2envs(props)
+        } else if (props["assetType"] == "AutoMl") {
+            doAutoML2envs(props)
+        } else if (props["assetType"] == "MlTrainer") {
+            doMLTrainer2envs(props)
+        } else {
+            error "Invalid asset type ${props['assetType']}"
+        }
     }
 }
